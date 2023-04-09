cc1312d121630ba69d117acdd29d315d7365eab2
JASaddington <jasaddington@gmail.com>
2023-04-08 19:54:23 -0500

    building question cycle

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,8 +1,28 @@
-// #1 question
-var h2El = document.getElementById("n-question");
-
-h2El.textContent = "Who is the main protaganist that you start out playing as";
-
-// # 1 answers
-
-// 
+// question objects array
+var allQuestions = [
+  {
+    ask: "What is the name of the main protagonist that you start playing as?",
+    answers: ["John", "Sara", "Arthur", "Caleb"],
+    correct: "Arthur"
+  },
+  {
+    ask: "What is best arrow to use to get perfect pelts from large animals?",
+    answers: ["Fire", "Exploding", "Regular", "Improved"],
+    correct: "Improved"
+  },
+  {
+    ask: "What is the name of the main protagonist that you start playing as?",
+    answers: ["John", "Sara", "Arthur", "Caleb"],
+    correct: "Arthur"
+  },
+  {
+    ask: "What is the name of the main protagonist that you start playing as?",
+    answers: ["John", "Sara", "Arthur", "Caleb"],
+    correct: "Arthur"
+  },
+  {
+    ask: "What is the name of the main protagonist that you start playing as?",
+    answers: ["John", "Sara", "Arthur", "Caleb"],
+    correct: "Arthur"
+  }
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,23 +3,57 @@ console.log("ding");
 
 var highscoresEl = document.querySelector("highscores");
 var timerEl = document.querySelector("timer");
-
 var startBtnEl = document.getElementById("startbtn");
+var askEl = document.getElementById("ask-question");
+var answersEl = document.getElementById("answers");
+
 
 // hide start screen when startbtn ("shoot!" button) clicked
 startBtnEl.onclick = function start() {
   var startPageEl = document.getElementById("start-page");
-  // if (startPageEl.style.display !== "none") {
-  //   startPageEl.style.display = "none";
-  //   }
+
+  // hides start page
   startPageEl.setAttribute("class", "hidden");
 
   // make question page appear
   var questionsPageEl = document.getElementById("question-page");
   questionsPageEl.removeAttribute("class", "hidden");
+
+  getQuestion();
 }
 
-function showQuestions() {
-  var nQuestionEl
+var questionIndex = 0;
+
+
+
+function getQuestion() {
+  
+  
+  // fetch question from question array
+  var currentQuestion = allQuestions[questionIndex];
+  askEl.textContent = currentQuestion.ask;
+  
+  // answers appear, forEach, loop
+  currentQuestion.answers.forEach(function(answer, i) {
+    var answerButton = document.createElement("button");
+    answerButton.setAttribute("class", "answer");
+    answerButton.setAttribute("value", "answer");
+    answerButton.textContent = i + 1 + answer;
+
+    // answers and events for answers
+    answerButton.onclick = chooseAnswer;
+    answersEl.appendChild(answerButton);
+  });
+
+  //clear old stuff
+  // answersEl.textContent = "";
+  // askEl.textContent = "";
 }
+
+function chooseAnswer() {
+  if (this.value !== allQuestions[questionIndex].correct) {
+    // time -=10;
+  }
+}
+
 
